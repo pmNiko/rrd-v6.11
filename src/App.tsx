fd7ec7492ffcc5c63ab40ec91bc101cc525806db
nikoDev <nikolas090189@gmail.com>
2023-05-27 16:28:13 -0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Box, Button, Container, Typography } from "@mui/material";
+import { Playground } from "./Menu/Menu";
+
+function App() {
+  return (
+    <Playground />
+    // <Container>
+    //   <h1>Main</h1>
+
+    //   <Box
+    //     border={2}
+    //     p={5}
+    //     borderColor={"peru"}
+    //     bgcolor={"#111"}
+    //     color={"white"}
+    //   >
+    //     <Typography variant="caption">
+    //       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea
+    //       reiciendis perspiciatis delectus quibusdam magnam soluta, rerum quos
+    //       eaque commodi dolorum itaque a provident fugit nostrum perferendis
+    //       nemo consequuntur! Ullam, eos!
+    //     </Typography>^
+    //     <Button variant="contained" sx={{ mt: 3 }}>
+    //       Mi boton
+    //     </Button>
+    //   </Box>
+    // </Container>
+  );
+}
+
+export default App;
