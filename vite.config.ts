be7b8be3368e54a0e5d7d2d543b1d9cce95642e1
nikoDev <nikolas090189@gmail.com>
2023-05-28 12:21:50 -0300

    Configuración de vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "192.168.1.107",
-    port: 3000,
-  },
+export default defineConfig(() => {
+  const env = loadEnv("mock", process.cwd(), "");
+  const processEnvValues = {
+    "process.env": Object.entries(env).reduce((prev, [key, val]) => {
+      console.log(key, val);
+      return {
+        ...prev,
+        [key]: val,
+      };
+    }, {}),
+  };
+
+  return {
+    base: "./",
+    plugins: [react()],
+
+    define: processEnvValues,
+    server: {
+      host: `${env.HOST || "localhost"}`,
+      port: 3000,
+    },
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
